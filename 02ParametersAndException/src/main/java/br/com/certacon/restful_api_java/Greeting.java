f445818188dbff6e8e17dbb4611e9a68be039c7e
Mariana Torres <mariana.torres@certacon.com.br>
2024-11-18 11:42:54 -0300

    Adiciona novo projeto

---
+++ 02ParametersAndException/src/main/java/br/com/certacon/restful_api_java/Greeting.java
@@ -0,0 +1,19 @@
+package br.com.certacon.restful_api_java;
+
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
---
+++ 02ParametersAndException/src/main/java/br/com/certacon/restful_api_java/RestfulApiJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.certacon.restful_api_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiJavaApplication.class, args);
+	}
+
+}
