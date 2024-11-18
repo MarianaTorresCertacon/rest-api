31655053c063ec0731e99abd59ff170d282b5871
Mariana Torres <mariana.torres@certacon.com.br>
2024-11-18 11:23:15 -0300

    Primeiro exemplo de aplicação em java

---
+++ src/test/java/br/com/certacon/restful_api_java/RestfulApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.certacon.restful_api_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
